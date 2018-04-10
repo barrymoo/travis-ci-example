182165724a2fe071446fd88e78e1b9fa07cab980
Barry Moore <moore0557@gmail.com>
2018-04-10 09:49:34 -0400

    Minor changes

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 
+#!/usr/bin/env bash
 cwd=$(pwd)
 
+# Generate compile_commands.json
 mkdir $cwd/build
 cd $cwd/build
-cmake -DCMAKE_CXX_COMPILER=g++ ..
+cmake -DCMAKE_CXX_COMPILER=g++ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+
+# Lint the code
+cd $cwd
+ln -s build/compile_commands.json
+clang-tidy hello.cpp --checks=clang-*,modernize-*
+
+# Build and run the code
+cd $cwd/build
 make
 ./hello
